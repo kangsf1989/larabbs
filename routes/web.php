38ccd37ce236c510b51fa77ac8a60f03c213477f
kangsf1989 <kangxingli1989@163.com>
2018-07-12 17:06:40 +0800

    generated topics

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 //结束
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
