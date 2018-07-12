899a5b68b51dbab3e4059f04a0477ddccb32bdfc
kangsf1989 <kangxingli1989@163.com>
2018-07-12 11:37:36 +0800

    finish basic page layout

---
+++ bootstrap/helpers.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @return mixed
+ * @content 调用 {{ route_class() }}-page
+ * 将当前请求的路由名称转换为 CSS 类名称，作用是允许我们针对某个页面做页面样式定制
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
