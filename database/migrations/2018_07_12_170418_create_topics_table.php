f3ea5f02fb51254e948ed8803812952400e5b7ad
kangsf1989 <kangxingli1989@163.com>
2018-07-12 17:12:01 +0800

    edit topics

--- database/migrations/2018_07_12_170418_create_topics_table.php
+++ database/migrations/2018_07_12_170418_create_topics_table.php
@@ -9,16 +9,16 @@ class CreateTopicsTable extends Migration
 	{
 		Schema::create('topics', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->index();
-            $table->text('body');
-            $table->integer('user_id')->unsigned()->index();
-            $table->integer('category_id')->unsigned()->index();
-            $table->integer('reply_count')->unsigned()->default(0);
-            $table->integer('view_count')->unsigned()->default(0);
-            $table->integer('last_reply_user_id')->unsigned()->default(0);
-            $table->integer('order')->unsigned()->default(0);
-            $table->text('excerpt');
-            $table->string('slug')->nullable();
+            $table->string('title')->index()->comment('标题');
+            $table->text('body')->comment('帖子内容');
+            $table->integer('user_id')->unsigned()->index()->comment('用户 ID');
+            $table->integer('category_id')->unsigned()->index()->comment('分类 ID');
+            $table->integer('reply_count')->unsigned()->default(0)->comment('回复数量');
+            $table->integer('view_count')->unsigned()->default(0)->comment('查看总数');
+            $table->integer('last_reply_user_id')->unsigned()->default(0)->comment('最后回复的用户 ID');
+            $table->integer('order')->unsigned()->default(0)->comment('可用来做排序使用');
+            $table->text('excerpt')->comment('文章摘要，SEO 优化时使用');
+            $table->string('slug')->nullable()->comment('SEO 友好的 URI');
             $table->timestamps();
         });
 	}
