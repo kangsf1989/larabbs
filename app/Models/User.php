dfc4d977a02aa41e598d5e7a4c0784e7de8a62bd
kangsf1989 <kangxingli1989@163.com>
2018-07-13 17:22:52 +0800

    list topics created by user

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,4 +27,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //话题与模型的关联
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
