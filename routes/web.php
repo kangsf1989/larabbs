f3ba1e25d4b3010133e53b963a6e48ac286a2d6e
kangsf1989 <kangxingli1989@163.com>
2018-07-16 11:52:18 +0800

    user can upload topic image

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,6 @@ Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'crea
 
 //分类列表
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+//上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
