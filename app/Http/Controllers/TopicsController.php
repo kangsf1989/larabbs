78a71a7e491582f137aa4f9ea650ce5e4611dea8
kangsf1989 <kangxingli1989@163.com>
2018-07-16 13:56:27 +0800

    author can delete user

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -63,7 +63,7 @@ class TopicsController extends Controller
         $this->authorize('destroy', $topic);
         $topic->delete();
 
-        return redirect()->route('topics.index')->with('message', '删除成功');
+        return redirect()->route('topics.index')->with('success', '成功删除！');
     }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
